92e9e75c7b4e4fa5bf480b5f3f47d197bfe48119
ilia1 <ilia19011991@gmail.com>
2023-02-26 23:40:22 +0300

    added attach

--- src/test/java/tests/android/selenide/TestBase.java
+++ src/test/java/tests/android/selenide/TestBase.java
@@ -3,15 +3,15 @@ package tests.android.selenide;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import drivers.BrowserstackDriver;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-public class TestBase {
+class TestBase {
 
     @BeforeAll
     static void beforeAll() {
@@ -27,9 +27,12 @@ public class TestBase {
 
     @AfterEach
     void addAttachments() {
-        /// добавить аттачи
+        String sessionId = sessionId().toString();
+
+//        Attach.screenshotAs("Last screenshot");
+//        Attach.pageSource();
+
         closeWebDriver();
+//        Attach.addVideo(sessionId);
     }
-
-
 }
